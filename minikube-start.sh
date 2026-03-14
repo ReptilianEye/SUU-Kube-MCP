25427ddc0c781ed2650674a9562f297386dc4271
reptilianeye <piotr.rzadkowski@swmansion.com>
2026-03-14 13:50:03 +0100

    remove start minikube script

--- minikube-start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Start Minikube with 8GB memory and 4 CPUs for OpenTelemetry Demo
-# minikube start --memory=8192 --cpus=4
-
-# Use max memory and CPUs
-minikube start --memory=max --cpus=max
